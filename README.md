cef93cf3f21e224fc71c3c2465c6eeb7870c14b1
Thomas Marek <tmarek@stripe.com>
2020-02-11 22:08:31 -0500

    Improved success page and README

---
+++ README.md
@@ -0,0 +1,20 @@
+# React Stripe.js Walkthrough
+
+![donut-shop](https://user-images.githubusercontent.com/59585336/74299251-111fb300-4d1a-11ea-932b-a6e7b33f6ea8.gif)
+
+### Getting Started
+
+- clone this repo locally by clicking on "Clone or Download"
+- from a terminal, cd into the project folder `cd react-stripe-js-demo`
+- checkout the getting started branch `git checkout getting-started`
+- install the dependencies `npm install`
+- start developing! `npm run dev`
+
+### Credit Card Input Design Requirements
+
+- The behavior, look and feel of the app should match the gif exactly.
+- Text color: `#fff`
+- Font size: `16px`
+- Placeholder text color: `#87bbfd`
+- Invalid text color: `#FFC7EE`
+- Success/Complete text color: `#cbf4c9`
--- pages/success.js
+++ pages/success.js
@@ -1,28 +1,43 @@
 import styled from "@emotion/styled";
+import Confetti from "react-confetti";
+import { useState, useEffect } from "react";
 
 import Layout from "../components/Layout";
 import Row from "../components/prebuilt/Row";
-import Image from "../components/prebuilt/Image";
+
+const Container = styled.div`
+  width: 475px;
+  margin: 30px auto 0 auto;
+  text-align: center;
+  color: #fff;
+`;
 
 const Title = styled.div`
-  text-align: center;
   font-size: 58px;
-  color: #fff;
-  margin-top: 30px;
 `;
 
 const Message = styled.div`
-  text-align: center;
-  color: #fff;
   margin-top: 40px;
 `;
 
 export default () => {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    }, 100);
+  });
+
   return (
     <Layout title="Success!">
-      <Image src="/parrot.gif" />
-      <Title>success</Title>
-      <Message>Stripe has successfully processed your payment</Message>
+      <Container>
+        <Confetti width={width} height={height} numberOfPieces={450} />
+        <Title>congrats!</Title>
+        <Message>Stripe has successfully processed your payment</Message>
+      </Container>
     </Layout>
   );
 };
