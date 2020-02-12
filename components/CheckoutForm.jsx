56798bb038080939615a530186c51272e56b9fbf
Thomas Marek <tmarek@stripe.com>
2020-02-11 22:25:46 -0500

    remove the fun stuff!

--- components/CheckoutForm.jsx
+++ components/CheckoutForm.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import styled from "@emotion/styled";
 import axios from "axios";
 
@@ -23,17 +22,6 @@ const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
   const [isProcessing, setProcessingTo] = useState(false);
   const [checkoutError, setCheckoutError] = useState();
 
-  const stripe = useStripe();
-  const elements = useElements();
-
-  // TIP
-  // use the cardElements onChange prop to add a handler
-  // for setting any errors:
-
-  const handleCardDetailsChange = ev => {
-    ev.error ? setCheckoutError(ev.error.message) : setCheckoutError();
-  };
-
   const handleFormSubmit = async ev => {
     ev.preventDefault();
 
@@ -47,78 +35,6 @@ const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
         postal_code: ev.target.zip.value
       }
     };
-
-    setProcessingTo(true);
-
-    const cardElement = elements.getElement("card");
-
-    try {
-      const { data: clientSecret } = await axios.post("/api/payment_intents", {
-        amount: price * 100
-      });
-
-      const paymentMethodReq = await stripe.createPaymentMethod({
-        type: "card",
-        card: cardElement,
-        billing_details: billingDetails
-      });
-
-      if (paymentMethodReq.error) {
-        setCheckoutError(paymentMethodReq.error.message);
-        setProcessingTo(false);
-        return;
-      }
-
-      const { error } = await stripe.confirmCardPayment(clientSecret, {
-        payment_method: paymentMethodReq.paymentMethod.id
-      });
-
-      if (error) {
-        setCheckoutError(error.message);
-        setProcessingTo(false);
-        return;
-      }
-
-      onSuccessfulCheckout();
-    } catch (err) {
-      setCheckoutError(err);
-    }
-  };
-
-  // Learning
-  // A common ask/bug that users run into is:
-  // How do you change the color of the card element input text?
-  // How do you change the font-size of the card element input text?
-  // How do you change the placeholder color?
-  // The answer to all of the above is to use the `style` option.
-  // It's common to hear users confused why the card element appears impervious
-  // to all their styles. No matter what classes they add to the parent element
-  // nothing within the card element seems to change. The reason for this is that
-  // the card element is housed within an iframe and:
-  // > styles do not cascade from a parent window down into its iframes
-
-  const iframeStyles = {
-    base: {
-      color: "#fff",
-      fontSize: "16px",
-      iconColor: "#fff",
-      "::placeholder": {
-        color: "#87bbfd"
-      }
-    },
-    invalid: {
-      iconColor: "#FFC7EE",
-      color: "#FFC7EE"
-    },
-    complete: {
-      iconColor: "#cbf4c9"
-    }
-  };
-
-  const cardElementOpts = {
-    iconStyle: "solid",
-    style: iframeStyles,
-    hidePostalCode: true
   };
 
   return (
@@ -127,17 +43,11 @@ const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
         <BillingDetailsFields />
       </Row>
       <Row>
-        <CardElementContainer>
-          <CardElement
-            options={cardElementOpts}
-            onChange={handleCardDetailsChange}
-          />
-        </CardElementContainer>
+        <CardElementContainer></CardElementContainer>
       </Row>
       {checkoutError && <CheckoutError>{checkoutError}</CheckoutError>}
       <Row>
-        {/* TIP always disable your submit button while processing payments */}
-        <SubmitButton disabled={isProcessing || !stripe}>
+        <SubmitButton disabled={isProcessing}>
           {isProcessing ? "Processing..." : `Pay $${price}`}
         </SubmitButton>
       </Row>
