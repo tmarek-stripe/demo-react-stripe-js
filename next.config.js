32941c392ba37063f17f07fccdb80843dbc1b9ce
Thomas Marek <tmarek@stripe.com>
2020-02-12 22:25:45 -0500

    Update secret key handling

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
+
 module.exports = {
   env: {
-    SECRET_KEY: "sk_test_xxxxx",
-    PUBLISHABLE_KEY: "pk_test_xxxxx"
+    PUBLISHABLE_KEY: process.env.PUBLISHABLE_KEY
   }
 };
